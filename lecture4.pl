c7bddd856c09332ee1a082fc5602c620ec745055
morrisonmburu <morrisonmburu7@gmail.com>
2020-02-20 09:32:12 +0300

    Prolog classroom lectures

--- lecture4.pl
+++ lecture4.pl
@@ -3,4 +3,13 @@ male(john).
 female(ann).
 likes(ann,john).
 mother(X):-female(X).
+father(X):-male(X).
+human(socrates).
+mortal(X):-human(X).
+carnivore(lion).
+eatsmeat(X):-carnivore(X).
+takes(njiru,cs2325,advancedartificialintelligence).
+takes(kiiru,cs2324,projectmanagement).
+taken(X,_,Z):-takes(X,_,Z).
+
 
