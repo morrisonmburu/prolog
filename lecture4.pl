226e8cd2a2e13122cd773dec130383a7fea0269d
morrisonmburu <morrisonmburu7@gmail.com>
2020-02-20 10:13:04 +0300

    Prolog classroom lectures

--- lecture4.pl
+++ lecture4.pl
@@ -8,8 +8,20 @@ human(socrates).
 mortal(X):-human(X).
 carnivore(lion).
 eatsmeat(X):-carnivore(X).
-takes(njiru,cs2325,advancedartificialintelligence).
-takes(kiiru,cs2324,projectmanagement).
+teaches(njiru,cs2325,advancedartificialintelligence).
+teaches(kiiru,cs2324,projectmanagement).
 taken(X,_,Z):-takes(X,_,Z).
 
+student(morris).
+student(olive).
+student(mburu).
+student(lucy).
 
+takes(morris,aai).
+takes(olive,aai).
+takes(mburu,sad).
+takes(lucy,sad).
+
+
+postgraduate(X):-student(X),takes(X,aai).
+undergraduate(X):-student(X),takes(X,sad).
